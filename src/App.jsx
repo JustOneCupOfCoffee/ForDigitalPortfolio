6543d59732769d6c03e9e1b7b40054886d96b7e7
Sage of Reasons : Nawinnaphat Fiat K. <fiat2531.com@gmail.com>
2025-06-24 10:12:46 +0000

    KU_Portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,33 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="KULogo.png" className="App-logo" alt="logo" />
+        <hr style={{ width: "100%", border: "4px solid #a88e1f" }} />
+
+        <img src="Untitled design.png" className="App-logo" alt="logo"/>
+
+        <hr style={{ width: "100%", border: "4px solid #a88e1f" }} />
+
+        <h3>👋 Hello Mr. or Ms. KU interviewer 💻</h3>
+
         <p>
-          Hello Mr. or Ms. KU interviewer,
+          It's an honor to present this digital portfolio, built with the knowledge I gained from your 'Application with React' course.
         </p>
-        <p className="medium">
-        It's an honor to present this project, built with the knowledge I gained from your 'Application with React' course.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
+
+        <h4>
+          Mr. Nawinnaphat Kanokkornpisult Present <br />
+          (นวินพรรษ กนกกรพิศุทธิ์)
+        </h4>
+<h2>🕵️ Personal Projects 🕵️ <br></br>
+        (โครงการส่วนบุคคล)</h2>
+        <h2>🌏 National Level 🌏 <br></br>
+        (ระดับประเทศ)</h2>
+        <h2>📌 District Level 📌<br></br>
+        (ระดับเขต)</h2>
+        <img src="image.png" class="IMAGESMALL2"/>
+        <h2>✐ Training Certificate
+ ✐<br></br>
+        (เกียรติบัตรอบรม)</h2>
+        <img src="ใบประกาศนียบัตร React-37.png" class="IMAGESMALL"/>
       </header>
     </div>
   );
